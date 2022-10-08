7cd5cb139cd8fdb32487822af490f71cdccdf3c8
Nermine Maddouri <62077915+NerminSMaddouri@users.noreply.github.com>
2022-10-08 15:46:06 +0100

    Add files via upload

---
+++ response.js
@@ -0,0 +1,79 @@
+const userTexts = [
+  ["aslema", "sbeh elkhir", "alaslemtek"],
+  ["chnahwelek", "chneya lmoumour", "labes alik ", "labes", "nchalla tkoun bkhir",
+   "are you fine", "how is your day going", "how is your day", "what's up", "whats up", "you good"],
+  ["fech taaml", "chaandek jdid", "fama chay jdid", "kelaada", "kima khalitek"],
+  ["how is your day", "how was your day", "how did your day go", "how's your day going",
+  "how's your day", "hows your day", "how's your day going", "how is your day going"],
+  ["how old are you", "are you old"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you", "were you created"],
+  [
+    "esmek yaaychek",
+    "esmek ?",
+    "najem naaref esmek",
+    "chnowa esmek ",
+    "chnowa taayet lrouhekf",
+    "chnowa ynediwek",
+    "chnowa naayetlek",
+    "do you have a name",
+    "koli ala esmek "
+  ],
+  ["nhebek", "taajebni"],
+  ["aya behi ", "mala jaw "],
+  ["bad", "bored", "tired", "sad"],
+  ["aaweni", "ahkili haja", "koli haja tdhahek ", "andek nokta"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "beslema", "filamen", "au revoir"],
+  ["chnowa nekel lyoum ena ", "zaama chnowa ntayeb"],
+  ["bro"],
+  ["chnowa", "aaleh", "kifeh", "win", "wakteh"],
+  ["le","manich metaked ","yomken","le yaaychek"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke","lmao"],
+  ["im ok", "im good", "im okay", "im fine", "good"]
+]
+
+const botReplies = [
+  ["ahla bik", "sbeh enour", "ysalmek", "chnowa ejaw","nharek zin "],
+  [
+    "labes w enty",
+    "yaaychek labes w enty labes " ,
+    "oumouri aal el aal "
+  ],
+  [
+    "chay ",
+    "kelaada",
+    "hata chay kima khalitni ",
+    "ken lkdim"
+  ],
+  ["labes andi barcha"],
+  ["ena  infinite"],
+  ["rani bot", "ena rani bot w bara"],
+  ["The one true God, JavaScript"],
+  ["masamwnich", "maandich esm"],
+  ["hatena..", "w ena zeda"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["aleh?", "aaleh chbik ", "tfarej f talvza"],
+  ["ala chnowa", "mara mel marat ... wfet hkeyti"],
+  ["ahkili hkeya", "ahkili nokta", "ahkili ala rouhek"],
+  ["Bye", "beslema", "filamen"],
+  ["kosksi", "makrouna"],
+  ["ahh ?"],
+  ["soueal behi"],
+  ["mch mochkla , ala chnowa tahki?"],
+  ["bellehi koul haja"],
+  ["Haha!,tahfouna menek !"],
+  ["mriguel", "good", "nice", "okay"]
+]
+  
+const alternative = [
+  "bara aad",
+  "haya aad",
+  "woh",
+  "aawed mara okhra",
+  "nasmaa fik",
+  "mafhemtch"
+] 
+
+
